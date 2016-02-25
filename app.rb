5264c1f342a10c9e21a954b20b621f7b192f60ec
Tijesunimi Peters <tije@starcityhq.zerofoursixty.com>
2016-02-25 17:20:54 +0100

    database crud working

---
+++ app.rb
@@ -0,0 +1,50 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/activerecord'
+require './environments'
+require "json"
+require 'sinatra/reloader'
+
+
+class User < ActiveRecord::Base
+  has_many :results, dependent: :destroy
+end
+
+class Result < ActiveRecord::Base
+  belongs_to :user
+end
+
+get '/' do
+  haml :index
+end
+
+post '/registration' do
+  return {:response => "invalid parameters"}.to_json if !request.accept? 'application/json'
+
+  data = JSON.parse request.body.string
+
+
+  user = User.new
+  user.username = data['username']
+  user.email = data['email']
+  user.password = data['password']
+
+  if user.save
+    {:response => "new user saved sucesful"}.to_json
+  else
+    {:response => "saving failed"}.to_json
+  end
+end
+
+post '/login' do
+    return {:response => "invalid parameters"}.to_json if !request.accept? 'application/json'
+    data = JSON.parse request.body.string
+
+    user = User.where(:email=>data['email'],:password=>data['password'])
+
+    if user.empty?
+      {:code=>'error',:response => "User not registered or password combination failed"}.to_json
+    end 
+
+    {:code=>'success',:response => 'Login Successful',:user=>user[0]}.to_json if !user.empty?
+end
