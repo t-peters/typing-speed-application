c62c8e6834675f000ab294d30757c376b60b972d
Tijesunimi Peters <tije@starcityhq.zerofoursixty.com>
2016-02-29 03:14:24 +0100

    rubocop chores

--- routes.rb
+++ routes.rb
@@ -3,10 +3,11 @@ get '/' do
 end
 
 post '/registration' do
-  return {:response => "invalid parameters"}.to_json if !request.accept? 'application/json'
+  unless request.accept? 'application/json'
+    { response: 'invalid parameters' }.to_json
+  end
 
   data = JSON.parse request.body.string
-
 
   user = User.new
   user.username = data['username']
@@ -14,30 +15,34 @@ post '/registration' do
   user.password = data['password']
 
   if user.save
-    {:response => "new user saved sucesful"}.to_json
+    { response: 'new user saved sucesful' }.to_json
   else
-    {:response => "saving failed"}.to_json
+    { response: 'saving failed' }.to_json
   end
 end
 
 post '/login' do
-    return {:response => "invalid parameters"}.to_json if !request.accept? 'application/json'
-    data = JSON.parse request.body.string
-
-    user = User.where(:email=>data['email'],:password=>data['password'])
-
-    if user.empty?
-      {:code=>'error',:response => "User not registered or password combination failed"}.to_json
-    end 
-
-    {:code=>'success',:response => 'Login Successful',:user=>user[0]}.to_json if !user.empty?
-end
-
-
-post '/save/result' do 
-  return {:response => "invalid parameters"}.to_json if !request.accept? 'application/json'
+  unless request.accept? 'application/json'
+    { response: 'invalid parameters' }.to_json
+  end
   data = JSON.parse request.body.string
 
-  data.to_json
+  user = User.where(email: data['email'], password: data['password'])
 
+  if user.empty?
+    { code: 'error', response: 'User not registered or \
+      password combination failed' }.to_json
+  end
+
+  unless user.empty?
+    { code: 'success', response: 'Login Successful', user: user[0] }.to_json
+  end
+end
+
+post '/save/result' do
+  unless request.accept? 'application/json'
+    { response: 'invalid parameters' }.to_json
+  end
+  data = JSON.parse request.body.string
+  data.to_json
 end
